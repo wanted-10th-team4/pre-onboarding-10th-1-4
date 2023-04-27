4c47ee1158e4a963019055a244003a0cc0b98275
JunSeong <gentlmanjun@naver.com>
2023-04-27 15:17:57 +0900

    feat: 허스키 테스트

--- src/api/test.tsx
+++ src/api/test.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
 export default function Api() {
-  return (<div>
-    bbb
-    </div>);
+  return <div>bbb</div>;
 }
--- src/components/aaa/abc.tsx
+++ src/components/aaa/abc.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export default function abc() {
   return <div>aa</div>;
 }

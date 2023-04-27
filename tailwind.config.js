582366a369081cf15543714f01c2b93e8c0042f8
5thwin <hisy2059@naver.com>
2023-04-27 20:28:29 +0900

    fix: tailwind 오류 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/*/.{js,jsx,ts,tsx}'],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#7895B2",
+        secondary: '#AEBDCA',
+        background: '#F5EFE6',
+        beige: '#E8DFCA',
+        bright: "#88A4C2",
+        anchor: '#5865CF'
+      },
+      keyframes: {
+        'bounce-right': {
+          '0%, 100%': {
+            transform: 'translateX(0)',
+          },
+          '50%': {
+            transform: 'translateX(10px);'
+          },
+        }
+      },
+      animation: {
+        'bounce-right': 'bounce-right 1s ease-in-out infinite'
+      }
+    },
   },
   plugins: [],
 }

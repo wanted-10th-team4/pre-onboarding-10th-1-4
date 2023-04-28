b1262e3c91d3bb383883bb808a893abd1a6b4524
JunSeong <gentlmanjun@naver.com>
2023-04-28 19:20:04 +0900

    refactor: api 중복 제거

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({ baseURL: process.env.REACT_APP_API_URL });
 
 api.interceptors.request.use(config => {
-  const token = localStorage.getItem('access_token');
+  const token = localStorage.getItem('token');
   config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
--- src/api/todo.ts
+++ src/api/todo.ts
@@ -1,4 +1,4 @@
-import { authInstance } from './instance';
+import api from './index';
 
 type Todo = {
   id: number;
@@ -22,20 +22,20 @@ type UpdateTodoResponse = Todo;
 type DeleteTodoResponse = void;
 
 export const createTodo = async (requestBody: CreateTodoRequestBody): Promise<CreateTodoResponse> => {
-  const response = await authInstance.post(`/todos`, requestBody);
+  const response = await api.post(`/todos`, requestBody);
   return response.data;
 };
 
 export const getTodos = async (): Promise<GetTodosResponse> => {
-  const response = await authInstance.get(`/todos`);
+  const response = await api.get(`/todos`);
   return response.data;
 };
 
 export const updateTodo = async (id: number, requestBody: UpdateTodoRequestBody): Promise<UpdateTodoResponse> => {
-  const response = await authInstance.put(`/todos/${id}`, requestBody);
+  const response = await api.put(`/todos/${id}`, requestBody);
   return response.data;
 };
 
 export const deleteTodo = async (id: number): Promise<DeleteTodoResponse> => {
-  await authInstance.delete(`/todos/${id}`);
+  await api.delete(`/todos/${id}`);
 };
